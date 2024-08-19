2c173319af67f52b8cda35a5d5fc29cf3d12f048
bpoche <brettdp@gmail.com>
2024-08-19 16:52:50 -0500

    feat: working crawl counting

--- crawl.js
+++ crawl.js
@@ -1,18 +1,14 @@
 import { JSDOM } from 'jsdom'
 
 function normalizeURL(urlString){
-    try {
-        const url = new URL(urlString)
-        const host = url.host
-        let path = url.pathname
-        if (path.slice(-1) === '/') {
-            path = path.slice(0, -1)
-        }
-        return (host + path).toLowerCase()
+    console.log("normazlizing",urlString)
+    const url = new URL(urlString)
+    const host = url.host
+    let path = url.pathname
+    if (path.slice(-1) === '/') {
+        path = path.slice(0, -1)
     }
-    catch {
-        console.log("Invalid URL")
-    }
+    return (host + path).toLowerCase()
 }
 
 function getURLsFromHTML(htmlBody, baseURL){
@@ -27,38 +23,81 @@ function getURLsFromHTML(htmlBody, baseURL){
                 href = new URL(href, baseURL).href
                 urls.push(href)
             } catch(err) {
-                console.log(`${err.message}: ${href}`)
+                console.log(`getURLSFromHTML: ${err.message}: ${href}`)
             }
         }
     }
     return urls
 }
 
-async function crawlPage(baseURL) {
-
+async function fetchPage(url) {
+    console.log("Fectching",url)
     let res
     try {
-        res = await fetch(baseURL)
+        res = await fetch(url)
     } catch (err) {
-        throw new Error(`Network error fetching the URL: ${err.message}`);
+        throw new Error(`Network error fetching the URL: ${url}, ${err.message}`);
     }
 
     // Check if the request was successful
-    if (!res.status > 399) {
+    if (res.status >= 400) {
         throw new Error(`HTTP error! Status: ${res.status}`);
     }
 
     const contentType = res.headers.get('content-type')
     if (!contentType || !contentType.includes('text/html')) {
-        console.log(`Not an HTML page, content-type: ${res.headers.get('content-type')}`);
+        console.log(`Not an HTML page, content-type: ${contentType}`);
     }
 
-    console.log(await res.text())
+    return await res.text()
+}
+
+//Initially called with base url
+async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+
+    //Avoid searching entire internet by making sure urls have same hosts
+    const currentURLObj = new URL(currentURL)
+    const baseURLObj = new URL(baseURL)
+    if (currentURLObj.hostname !== baseURLObj.hostname) {
+        console.log(`hosts do not match, base: ${baseURL}, current: ${currentURL} skipping...`)
+        return pages
+    }
+
+    //http:// prefix treated same as https:// etc.
+    const normalizedURL =  normalizeURL(currentURL)
+
+    // increment page counter if already visited, or initialize to 1
+    if (pages[normalizedURL] > 0) {
+        pages[normalizedURL]++
+        return pages
+    }
+
+    // Initialize page count to 1
+    pages[normalizedURL] = 1
+
+    // Fetch and parse html for currentURL
+    console.log(`crawling ${currentURL}`)
+    let htmlBody
+    try {
+        htmlBody = await fetchPage(currentURL)
+    } catch (err) {
+        //throw new Error(`Failed to get html, ${err.message}`)
+        console.log(`${err.message}`)
+        return pages
+    }
+
+    // Loop through internal links and recusively call crawlPage
+    const urls = getURLsFromHTML(htmlBody, baseURL)
+    for (const newURL of urls){
+        pages =  await crawlPage(baseURL, newURL, pages)
+    }
+    return pages
 }
 
 
 export { 
     normalizeURL,
     getURLsFromHTML,
+    fetchPage,
     crawlPage
 }
--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 import { argv } from 'node:process';
 import { 
     normalizeURL,
-    getURLsFromHTML,
     crawlPage
 } from "./crawl.js";
 
-function main() {
+async function main() {
     const args = argv.slice(2)
     if (args.length === 1) {
         const baseURL = args[0]
         console.log(`Starting crawl of: ${baseURL}...`)
-        crawlPage(baseURL)
+        const normalizedBaseURL = normalizeURL(baseURL)
+        if (!normalizedBaseURL) {
+            console.log("Invalid base URL provided. Exiting...")
+            return
+        }
+        const pages = await crawlPage(baseURL)
+        console.log(pages)
         return
     }
-    console.log(`whoops`)
+    console.log(`Usage: node main.js <url>`)
 }
 
-main();
+await main();
